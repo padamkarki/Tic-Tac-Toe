ae1d7fcd08e5503184c0f58e3b0389fa3af57756
Padam Karki <padamkarkipkkp@gmail.com>
2024-01-05 17:18:06 +0530

    func for first player random choice

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -41,6 +41,7 @@ def place_marker(board,marker,position):
 place_marker(test_board,'$',8)
 display_board(test_board)
 
+# Win check
 def win_check(board, mark):
     # all rows
     return ((board[1] == board[2] == board[3] == mark) or
@@ -53,3 +54,19 @@ def win_check(board, mark):
     # 2 diagnols
     (board[1] == board[5] == board[9] == mark) or
     (board[7] == board[5] == board[3] == mark))
+
+display_board(test_board)
+win_check(test_board,'X')
+
+
+# Choose first player turn
+import random
+def choose_first():
+    flip = random.randint(0,1)
+
+    if flip == 0:
+        return "Player 1"
+    else:
+        return "Player 2"
+    
+
