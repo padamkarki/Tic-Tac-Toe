d930c19b9082f66889ce038a7d74a757ae68008c
Padam Karki <padamkarkipkkp@gmail.com>
2024-01-05 17:14:30 +0530

    adds func for placing marker and win check

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,7 +7,10 @@ def display_board(board):
     print("-----")
     print(board[1] +"|"+ board[2] +"|"+ board[3])
 
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+display_board(test_board)
 
+# Takes players input and stores
 def player_input():
     marker = ""
     acceptable_mark =["X","O"]
@@ -30,3 +33,23 @@ def player_input():
     return (p1,p2)
 
 p1_marker, p2_marker = player_input()
+
+# Assigns it to the board
+def place_marker(board,marker,position):
+    board[position] = marker
+
+place_marker(test_board,'$',8)
+display_board(test_board)
+
+def win_check(board, mark):
+    # all rows
+    return ((board[1] == board[2] == board[3] == mark) or
+    (board[4] == board[5] == board[6] == mark) or
+    (board[7] == board[8] == board[9] == mark) or
+    # all col
+    (board[1] == board[4] == board[7] == mark) or
+    (board[2] == board[5] == board[8] == mark) or
+    (board[3] == board[6] == board[9] == mark) or
+    # 2 diagnols
+    (board[1] == board[5] == board[9] == mark) or
+    (board[7] == board[5] == board[3] == mark))
