776ee1e12c0a739b5f827bbe612d937609749ddf
Padam Karki <padamkarkipkkp@gmail.com>
2024-01-05 17:08:28 +0530

    player input and display board function added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,8 +7,26 @@ def display_board(board):
     print("-----")
     print(board[1] +"|"+ board[2] +"|"+ board[3])
 
-# test_board = ['#','X','O','X','O','X','O','X','O','X']
-# display_board(test_board)
 
 def player_input():
+    marker = ""
+    acceptable_mark =["X","O"]
+
+    while marker != "X" and marker != "O":
+        marker = input("Please choose your marker X or O: ").upper()
+
+        # Check Validity
+        if marker not in acceptable_mark:
+            print("Please choose either X or O: ")
+
+        #Assign input to V
+        p1 = marker
+
+        if p1 == "X":
+            p2 = "O"
+        else:
+            p2 = "X"
+
     return (p1,p2)
+
+p1_marker, p2_marker = player_input()
